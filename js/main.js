7926f42044592f4017727f189be8aa4a019146d9
ii buana <125364936+iibuana@users.noreply.github.com>
2026-02-06 23:32:17 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -116,8 +116,8 @@ document.addEventListener('DOMContentLoaded', () => {
                 videos: [
                     "https://youtube.com/shorts/v2cWKYKMazY?feature=share", // STANDARD VIDEO (4K Nature) for testing
                     "https://youtube.com/shorts/FlMNpQ1Rku0?feature=share",
-                    "https://youtube.com/shorts/97zgS5Lc9N4?feature=share",
                     "https://youtube.com/shorts/4gqtaBUlTEY?feature=share",
+                    "https://youtube.com/shorts/97zgS5Lc9N4?feature=share",
                     "https://youtube.com/shorts/R4XVyfdvLdI",
                     "https://youtube.com/shorts/j5aY29AmNsg?feature=share",
                     "https://youtu.be/ckeCT3sEp3Q", // STANDARD VIDEO (4K Nature) for testing
